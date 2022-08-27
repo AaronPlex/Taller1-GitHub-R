8131a541d349fd89481d28280fa87196b7607422
AaronPlex <77115605+AaronPlex@users.noreply.github.com>
2022-08-27 10:48:08 -0400

    Primera prueba

---
+++ codigo1.R
@@ -0,0 +1,18 @@
+
+# #########################################
+#              Taller de GitHub con R
+# #########################################
+
+
+# Introduccion a GitHub
+# 1. Instalar Git
+# 2. Crear un repositorio desde GitHub
+# 3. Crear un proyecto con version control
+# 4. Clornar repositorio a la computadora
+
+# Crear codigo
+df <- mtcars
+names(mtcars)
+
+summary(mtcars)
+plot(df$hp, main = "Grafico")
